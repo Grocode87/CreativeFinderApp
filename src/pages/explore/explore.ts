6c6211e109ea51b4dc8e7fd57339f3697fa8a776
Grocode87 <colin@grob.ca>
2018-12-26 16:04:05 -0800

    Added Explore Filtering basic functionality"

--- src/pages/explore/explore.ts
+++ src/pages/explore/explore.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SearchResultsPage } from '../search-results/search-results'
 
 /**
  * Generated class for the ExplorePage page.
@@ -14,12 +15,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'explore.html',
 })
 export class ExplorePage {
+    
+    toggled: boolean;
+    filterTypeOne: any;
+    filterTypeTwo: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor( public navCtrl: NavController, public navParams: NavParams ) {
+        this.toggled = false; 
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ExplorePage');
-  }
+        this.filterTypeOne = ["Newest", "Popular", "Oldest", "Most Viewed"]
+        this.filterTypeTwo = ["All", "PVP", "Obstacle Course", "Race", "Hide and Seek", "The Block", "Free Play/Other"]
+    }
+
+    ionViewDidLoad() {
+        console.log( 'ionViewDidLoad HomePage' );
+    } 
+
+    toggleSearch() {
+        this.toggled = this.toggled ? false : true;
+    }
+
+    searchQuery(searchbar: any) {
+      const val = searchbar.target.value;
+
+    
+      if (val && val.trim() != '') {
+        console.log(val)
+        this.navCtrl.parent.parent.push(SearchResultsPage, {
+            query: val
+          })
+      }
+    }
+
+    selectFilter(test1, test2) {
+      console.log(test1, test2)
+      +
+    }
 
 }
