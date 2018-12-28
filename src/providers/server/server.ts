6b4e590ccf636203646fddd1ba25baaa154e7012
Grocode87 <colin@grob.ca>
2018-12-28 01:58:11 -0800

    integrated pythonanywhere api instead of local

--- src/providers/server/server.ts
+++ src/providers/server/server.ts
@@ -10,7 +10,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ServerProvider {
 
-  apiUrl = 'http://127.0.0.1:5000'
+  apiUrl = 'http://cgrob10.pythonanywhere.com'
 
   constructor(public http: HttpClient) {}
 
